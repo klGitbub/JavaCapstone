4a78b8e9d60a99b4a1668a0a8a66221b82205c27
klGitbub <klaghaeian@gmail.com>
2023-04-16 13:26:45 -0400

    "New commit"

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,7 +2,7 @@
 const addPublisherForm = document.getElementById("add-publisher-form");
 addPublisherForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const publisherName = document.getElementById("publisher-name")
+    const publisherName=  document.getElementById("publisher-name")
     const publisherData = {
         name: publisherName.value
     };
@@ -18,23 +18,23 @@ addPublisherForm.addEventListener("submit", (event) => {
             alert("Failed to add publisher");
         });
 });
-// Get list of Publishers
-function getPublisherLists() {
+// Get Publishers
+function getPublisherLists(){
     const publisherList = document.getElementById("publisher-list");
-    // Remove all child elements of publisher list
+    // Remove all child elements of the publisher list
     while (publisherList.firstChild) {
         publisherList.removeChild(publisherList.firstChild);
     }
     axios.get("/publishers")
         .then(response => {
             response.data.forEach(function(publisher) {
-                // Create a new list item for each user
+                // Create a new list item element for each user
                 const listItem = document.createElement('li');
 
-                // Set the inner text of the li to the publisher's name
+                // Set the inner text of the list item to the user's name
                 listItem.innerText = publisher.name;
 
-                // Add the li to the publisher list
+                // Add the list item to the publisher list
                 publisherList.appendChild(listItem);
             });
         })
@@ -44,9 +44,10 @@ function getPublisherLists() {
         });
 }
 
-//Add Game Publisher
-function addGamePublisherLists() {
-    const publisherSelect = document.getElementById("game-publisher");
+// Add Game Publisher
+function addGamePublisherLists(){
+
+    const publisherSelect = document.getElementById("game-publishers");
     console.log(publisherSelect, "publisher===>")
     // Clear the previous options
     publisherSelect.innerHTML = "";
@@ -61,8 +62,10 @@ function addGamePublisherLists() {
             });
         })
 }
-// Add game list items
-function addGameLists() {
+
+// Add Game Lists
+function addGameLists(){
+
     const gameListElement = document.getElementById("game-list");
     while (gameListElement.firstChild) {
         gameListElement.removeChild(gameListElement.firstChild);
@@ -71,12 +74,11 @@ function addGameLists() {
     axios.get("/games")
         .then(response => {
             response.data.forEach(function(game) {
-                //Create a new list tem for each game
+                // Create a new list item element for each game
                 const listItem = document.createElement('li');
 
-                //Set the inner text of the item to the game's name
-                listItem.innerHTML = `Name: ${game.game_name} <br/>  ${game.game_developer}
-<br /> Genre: ${game.genre} <br/> Platform: ${game.game_platform} `;
+                // Set the inner text of the list item to the game's name
+                listItem.innerHTML = `${game.game_name} <br /> Developer: ${game.game_developer} <br /> Genre: ${game.genre} <br/> Platform: ${game.game_platform} `;
 
                 // Add the list item to the game list
                 gameListElement.appendChild(listItem);
@@ -85,7 +87,7 @@ function addGameLists() {
 }
 
 // Add Player Lists
-function addPlayerLists() {
+function addPlayerLists(){
 
     const playerListElement = document.getElementById("player-list");
     while (playerListElement.firstChild) {
@@ -95,43 +97,44 @@ function addPlayerLists() {
     axios.get("/players")
         .then(response => {
             response.data.forEach(function(player) {
-                //Crete a new li element for each player
+                // Create a new list item element for each player
                 const listItem = document.createElement('li');
 
                 // Set the inner text of the list item to the player's name
-                listItem.innerHTML = `Name: ${player.first_name} ${player.last_name} <br />
-Gender: ${player.gender} <br /> Age: ${player.age} `;
+                listItem.innerHTML = `Name: ${player.first_name} ${player.last_name} <br /> Gender: ${player.gender} <br /> Age: ${player.age} `;
 
-                //Add the list item to the game list
+                // Add the list item to the game list
                 playerListElement.appendChild(listItem);
             });
         })
 }
 
-//Add Player and Game Lists
-function addPlayerGameLists() {
+// Add Players Games Lists
+function addPlayerGameLists(){
+
     const playersList = document.getElementById("players-games-list");
     while (playersList.firstChild) {
         playersList.removeChild(playersList.firstChild);
     }
+
     axios.get("/players")
         .then(response => {
             const players = response.data;
-            // Loop through each player on the list
+            // Loop through each player
             players.forEach(player => {
                 const playerItem = document.createElement("li");
                 playerItem.innerHTML = `${player.first_name} ${player.last_name}`;
 
                 const gamesList = document.createElement("ul");
                 player.games.forEach(game => {
-                   const gameItem = document.createElement("li");
-                   gameItem.innerHTML = `${game.game_name} - ${game.genre}`;
-                   gamesList.appendChild(gameItem);
+                    const gameItem = document.createElement("li");
+                    gameItem.innerHTML = `${game.game_name} - ${game.genre}`;
+                    gamesList.appendChild(gameItem);
                 });
 
                 playerItem.appendChild(gamesList);
                 playersList.appendChild(playerItem);
-            })
+            });
         })
 }
 
@@ -141,11 +144,11 @@ addGameForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(addGameForm);
     const gameData = {
-        game_name: formData.get("game-name"),
+        game_name: formData.get("game_name"),
         publisher_id: formData.get("publisher_id"),
-        game_developer: formData.get("game-developer"),
-        genre: formData.get("game-genre"),
-        game_platform: formData.get("game-platform")
+        game_developer: formData.get("game_developer"),
+        genre: formData.get("genre"),
+        game_platform: formData.get("game_platform")
     };
     axios.post("/games", gameData)
         .then(response => {
@@ -181,7 +184,7 @@ addPlayerForm.addEventListener("submit", (event) => {
             alert("Failed to add player");
         });
 });
-// Add a game to a player
+// Add a player to a game
 const addGameToPlayerForm = document.getElementById("add-player-to-game");
 addGameToPlayerForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -190,7 +193,7 @@ addGameToPlayerForm.addEventListener("submit", (event) => {
         player_id: formData.get("player-id"),
         game_id: formData.get("game-id")
     };
-    axios.post(`/players${gameToPlayerData.player_id}/game${gameToPlayerData.game_id}`)
+    axios.post(`/players/${gameToPlayerData.player_id}/game/${gameToPlayerData.game_id}`)
         .then(response => {
             console.log(response.data);
             alert("Game added to player successfully");
@@ -202,66 +205,72 @@ addGameToPlayerForm.addEventListener("submit", (event) => {
         });
 });
 
-//main code
+
+// main
 (()=>{
-    //Select all links
+    // Select all menu links
     const menuLinks = document.querySelectorAll('.menu a');
     const menuItems = document.querySelectorAll('.menu li');
 
-    //Active class
+    // add active class
     menuItems[0].classList.add('active');
 
-    //Select all sections
+
+// Select all content sections
     const contentSections = document.querySelectorAll('.section > div');
 
-    //Loop through contentSections: Hide all except first one when page loads
+    // Loop through contentSections: Hide all except first one when page loads
     for (let i = 1; i < contentSections.length; i++) {
-        //Set display to none
+        // Set display to none
         contentSections[i].style.display = 'none';
     }
 
-    //Add click event listener to each menu link
+    // Add click event listener to each menu link
     menuLinks.forEach((link, index) => {
 
-        link.addEventListener('click', (event) => {
-            event.preventDefault(); //prevents default behavior
 
-            //remove active elements from, all previous menus
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); // prevent default link behavior
+
+            // remove active from all prev menus
             menuItems.forEach(menu => {
                 menu.classList.remove('active');
             });
-                // add active to current menu
+
+            // add active to current menu
             menuItems[index].classList.add('active');
 
+
             const targetSection = document.querySelector(link.hash);
-            switch (targetSection.id) {
+            switch (targetSection.id){
                 case "publishers-list":
                     getPublisherLists();
                     break;
                 case "add-game-form":
-                    addGamePublisherLists();
+                    addGamePublisherLists()
                     break;
                 case "games-list":
-                    addGameLists();
+                    addGameLists()
                     break;
                 case "players-list":
-                    addPlayerLists();
+                    addPlayerLists()
                     break;
                 case "games-and-players-list":
-                    addPlayerGameLists();
+                    addPlayerGameLists()
                     break;
                 default:
                     break;
+
             }
             console.log(targetSection, "target section")
 
-            // selects corresponding content section
+            // select corresponding content section
             if (targetSection) {
-                // Hides all content sections
+                // Hide all content sections
                 contentSections.forEach((section) => {
                     section.style.display = 'none';
                 });
-                // Displays target content section
+                // Display target content section
                 targetSection.style.display = 'block';
             }
         });
